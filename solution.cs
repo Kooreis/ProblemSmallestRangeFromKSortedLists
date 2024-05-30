4634ee7e9b8b238e0288f3311e2a8f263d4a0e0d
Kooreis <kooreis.official@gmail.com>
2024-05-29 22:54:34 -0500

    In this update, the function 'FindSmallestRange' is called with the list of lists as an argument. The result is then printed to the console.

--- solution.cs
+++ solution.cs
@@ -1,15 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+var result = FindSmallestRange(lists);
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        List<List<int>> lists = new List<List<int>>
-        {
-            new List<int> {4, 10, 15, 24, 26},
-            new List<int> {0, 9, 12, 20},
-            new List<int> {5, 18, 22, 30}
-        };
-    }
+        Console.WriteLine($"Smallest range is: [{result.Item1}, {result.Item2}]");
