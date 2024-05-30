888eab83401e09bc271c6c478ccce41b2c66a091
Kooreis <kooreis.official@gmail.com>
2024-05-29 22:54:34 -0500

    This update adds the main method which initializes the lists and calls the findSmallestRange method to find the smallest range. The result is then printed to the console.

--- solution.java
+++ solution.java
@@ -1,11 +1,10 @@
-import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        List<List<Integer>> lists = new ArrayList<>();
+        lists.add(Arrays.asList(4, 7, 9, 12, 15));
+        lists.add(Arrays.asList(0, 8, 10, 14, 20));
+        lists.add(Arrays.asList(6, 12, 16, 30, 50));
 
-class Node {
-    int elementIndex;
-    int arrayIndex;
-
-    Node(int elementIndex, int arrayIndex) {
-        this.elementIndex = elementIndex;
-        this.arrayIndex = arrayIndex;
+        int[] result = findSmallestRange(lists);
+        System.out.println("Smallest range is: [" + result[0] + ", " + result[1] + "]");
     }
-}
